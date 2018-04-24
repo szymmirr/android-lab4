18cf3722b607ba45cc7e28cb1bb81d2e83c53e35
Your Name <you@example.com>
2018-04-24 11:21:53 +0200

    done?

---
+++ app/src/main/java/com/example/student/android_lab/DodajWpis.java
@@ -0,0 +1,31 @@
+package com.example.student.android_lab;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class DodajWpis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_wpis);
+    }
+
+    public void wyslij (View view)
+    {
+        EditText kontrolka =
+                (EditText)findViewById(R.id.editText);
+        String pole =
+                kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);
+        finish();
+    }
+
+
+
+}
--- app/src/main/java/com/example/student/android_lab/MainActivity.java
+++ app/src/main/java/com/example/student/android_lab/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.student.android_lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -31,6 +35,33 @@ public class MainActivity extends AppCompatActivity {
         //nic
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode==1 && resultCode==RESULT_OK)
+        {
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
 
 
 }
